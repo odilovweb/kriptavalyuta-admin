4f53bc2e7423d96ed6a1bb306adb4a0911e027f1
odilovweb <mirzohidodilov49@gmail.com>
2024-05-26 20:53:07 +0500

    done

---
+++ index.js
@@ -0,0 +1,73 @@
+const { Telegraf } = require("telegraf");
+const { message } = require("telegraf/filters");
+
+const bot = new Telegraf("7488158255:AAHblSmJm7AMzEIkw0JBWc2Yeh24WIpJqkc");
+bot.on("message", async (ctx) => {
+  console.log(ctx.message.text);
+  if (
+    ctx.message.text.toLowerCase().includes("salom") ||
+    ctx.message.text.toLowerCase().includes("assolom alekum") ||
+    ctx.message.text.toLowerCase().includes("assolomu alekum") ||
+    ctx.message.text.toLowerCase().includes("assolomu aleykum") ||
+    ctx.message.text.toLowerCase().includes("assolomu alaykum")
+  ) {
+    try {
+      await ctx.reply("Assolumu alaykum yaxshimisiz 😊", {
+        reply_to_message_id: ctx.message.message_id,
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  } else if (
+    ctx.message.text.toLowerCase().includes("notcoin sotaman") ||
+    ctx.message.text.toLowerCase().includes("notkoin sotaman") ||
+    ctx.message.text.toLowerCase().includes("notkoyn sotaman") ||
+    ctx.message.text.toLowerCase().includes("notcoin sotiladi") ||
+    ctx.message.text.toLowerCase().includes("notkoin sotiladi") ||
+    ctx.message.text.toLowerCase().includes("notkoyn sotiladi")
+  ) {
+    try {
+      await ctx.reply(
+        `Agarda notcoin sotmoqchi bo'lsangiz @Kriptavalyuta_admin ga murojaat qiling. 
+Qimmat narxda sotib oladi va 100% ishonchli ✅`,
+        {
+          reply_to_message_id: ctx.message.message_id,
+        }
+      );
+    } catch (e) {
+      console.log(e);
+    }
+  } else if (ctx.message.text.toLowerCase().includes("admin")) {
+    try {
+      await ctx.reply(`Admin: @Kriptavalyuta_admin ✅`, {
+        reply_to_message_id: ctx.message.message_id,
+        reply_markup: {
+          inline_keyboard: [
+            [{ text: "Admin", url: "https://t.me/kriptavalyuta_admin" }],
+          ],
+        },
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+});
+setInterval(async () => {
+  const chatId = "-1948659067";
+  try {
+    await bot.telegram.sendMessage(
+      chatId,
+      `<b>Hurmatli guruh a'zolari !</b> 👋
+
+Kimda kim notcoin sotmoqchi bo'lsa ishonchli sotib berish hizmati mavjud.
+  
+⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️
+  
+Murojaat: @Kriptavalyuta_admin ✅`,
+      { parse_mode: "HTML" }
+    );
+  } catch (e) {
+    console.log(e);
+  }
+}, 15 * 60 * 1000);
+bot.launch();
