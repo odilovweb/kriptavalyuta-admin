8d56999f99ffca763db14ccd9b1494ce344211aa
odilovweb <mirzohidodilov49@gmail.com>
2024-05-26 22:05:04 +0500

    edited

--- index.js
+++ index.js
@@ -2,54 +2,56 @@ const { Telegraf } = require("telegraf");
 const { message } = require("telegraf/filters");
 
 const bot = new Telegraf("7488158255:AAHblSmJm7AMzEIkw0JBWc2Yeh24WIpJqkc");
-let time = 15;
+let time = 3;
 bot.on("message", async (ctx) => {
   console.log(ctx.chat.id);
-  if (
-    ctx.message.text.toLowerCase().includes("salom") ||
-    ctx.message.text.toLowerCase().includes("assolom alekum") ||
-    ctx.message.text.toLowerCase().includes("assolomu alekum") ||
-    ctx.message.text.toLowerCase().includes("assolomu aleykum") ||
-    ctx.message.text.toLowerCase().includes("assolomu alaykum")
-  ) {
-    try {
-      await ctx.reply("Assolumu alaykum yaxshimisiz 😊", {
-        reply_to_message_id: ctx.message.message_id,
-      });
-    } catch (e) {
-      console.log(e);
-    }
-  } else if (
-    ctx.message.text.toLowerCase().includes("notcoin sotaman") ||
-    ctx.message.text.toLowerCase().includes("notkoin sotaman") ||
-    ctx.message.text.toLowerCase().includes("notkoyn sotaman") ||
-    ctx.message.text.toLowerCase().includes("notcoin sotiladi") ||
-    ctx.message.text.toLowerCase().includes("notkoin sotiladi") ||
-    ctx.message.text.toLowerCase().includes("notkoyn sotiladi")
-  ) {
-    try {
-      await ctx.reply(
-        `Agarda notcoin sotmoqchi bo'lsangiz @Kriptavalyuta_admin ga murojaat qiling. 
-Qimmat narxda sotib oladi va 100% ishonchli ✅ `,
-        {
+  if (ctx.message.text.toLowerCase()) {
+    if (
+      ctx.message.text.toLowerCase().includes("salom") ||
+      ctx.message.text.toLowerCase().includes("assolom alekum") ||
+      ctx.message.text.toLowerCase().includes("assolomu alekum") ||
+      ctx.message.text.toLowerCase().includes("assolomu aleykum") ||
+      ctx.message.text.toLowerCase().includes("assolomu alaykum")
+    ) {
+      try {
+        await ctx.reply("Assolumu alaykum yaxshimisiz 😊", {
           reply_to_message_id: ctx.message.message_id,
-        }
-      );
-    } catch (e) {
-      console.log(e);
-    }
-  } else if (ctx.message.text.toLowerCase().includes("admin")) {
-    try {
-      await ctx.reply(`Admin: @Kriptavalyuta_admin ✅`, {
-        reply_to_message_id: ctx.message.message_id,
-        reply_markup: {
-          inline_keyboard: [
-            [{ text: "Admin", url: "https://t.me/kriptavalyuta_admin" }],
-          ],
-        },
-      });
-    } catch (e) {
-      console.log(e);
+        });
+      } catch (e) {
+        console.log(e);
+      }
+    } else if (
+      ctx.message.text.toLowerCase().includes("notcoin sotaman") ||
+      ctx.message.text.toLowerCase().includes("notkoin sotaman") ||
+      ctx.message.text.toLowerCase().includes("notkoyn sotaman") ||
+      ctx.message.text.toLowerCase().includes("notcoin sotiladi") ||
+      ctx.message.text.toLowerCase().includes("notkoin sotiladi") ||
+      ctx.message.text.toLowerCase().includes("notkoyn sotiladi")
+    ) {
+      try {
+        await ctx.reply(
+          `Agarda notcoin sotmoqchi bo'lsangiz @Kriptavalyuta_admin ga murojaat qiling. 
+  Qimmat narxda sotib oladi va 100% ishonchli ✅ `,
+          {
+            reply_to_message_id: ctx.message.message_id,
+          }
+        );
+      } catch (e) {
+        console.log(e);
+      }
+    } else if (ctx.message.text.toLowerCase().includes("admin")) {
+      try {
+        await ctx.reply(`Admin: @Kriptavalyuta_admin ✅`, {
+          reply_to_message_id: ctx.message.message_id,
+          reply_markup: {
+            inline_keyboard: [
+              [{ text: "Admin", url: "https://t.me/kriptavalyuta_admin" }],
+            ],
+          },
+        });
+      } catch (e) {
+        console.log(e);
+      }
     }
   }
 });
